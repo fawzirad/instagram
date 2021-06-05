a8e81d91f7975ca43806487ebd549ac0c7fe28dc
fawzirad <fawziradad7@gmail.com>
2021-06-05 13:48:27 +0100

    signup cree

--- src/App.js
+++ src/App.js
@@ -1,36 +1,120 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Post from './Post';
-import { db } from './firebase';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Post from "./Post";
+import { db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const signUp = (event) => {};
 
   // UseEffect --> Runs a piece of code based on a specific condition
-
   useEffect(() => {
-      // this is where the code runs
-    db.collection('posts').onSnapshot(snapshot => {
+    // this is where the code runs
+    db.collection("posts").onSnapshot((snapshot) => {
       // every time a new post is added, this code fires...
-      console.log("message", snapshot.docs.map(doc => doc.data()));
-      setPosts(snapshot.docs.map(doc => doc.data()));
-    })
+      console.log(
+        "message",
+        snapshot.docs.map((doc) => doc.data())
+      );
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
   }, []);
 
   return (
     <div className="App">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form>
+          <center>
+            <img
+              className="app__headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+              alt="instagram"
+            />
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
+            </center>
+          </form>
+
+          
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app__header">
-        <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt="instagram"/>
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+          alt="instagram"
+        />
       </div>
-      <h1>
-        Hello Instagram Clone !
-      </h1>
 
-      {
-        posts.map(post => (<Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>))
-      }
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
-      
+      <h1>Hello Instagram Clone !</h1>
+
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
