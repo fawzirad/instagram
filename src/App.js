fbf863216efe919f88ad344859679a9757e39b0d
fawzirad <fawziradad7@gmail.com>
2021-07-28 18:30:39 +0100

    youness a modifier

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import { Button, Input } from "@material-ui/core";
 import ImageUpload from "./ImageUpload";
+import InstagramEmbed from "react-instagram-embed";
 
 function getModalStyle() {
   const top = 50;
@@ -95,25 +96,25 @@ function App() {
   // UseEffect --> Runs a piece of code based on a specific condition
   useEffect(() => {
     // this is where the code runs
-    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      // every time a new post is added, this code fires...
-      console.log(
-        "message",
-        snapshot.docs.map((doc) => doc.data())
-      );
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          post: doc.data(),
-        }))
-      );
-    });
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        // every time a new post is added, this code fires...
+        console.log(
+          "message",
+          snapshot.docs.map((doc) => doc.data())
+        );
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            post: doc.data(),
+          }))
+        );
+      });
   }, []);
 
   return (
     <div className="App">
-      
-
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
           <form className="app__signup">
@@ -185,34 +186,49 @@ function App() {
           alt="instagram"
         />
         {user ? (
-        <Button onClick={() => auth.signOut()}>Logout</Button>
-      ) : (
-        <div className="app__loginContainer">
-          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
-          <Button onClick={() => setOpen(true)}>Sign Up</Button>
-        </div>
-      )}
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
 
-      
-
-      <h1>Hello Fawzi RADAD instagram clone!</h1>
-
-      {posts.map(({ id, post }) => (
-        <Post
-          key={id}
-          username={post.username}
-          caption={post.caption}
-          imageUrl={post.imageUrl}
-        />
-      ))}
+      <div className="app__posts">
+        <div className="app__postsLeft">
+          {posts.map(({ id, post }) => (
+            <Post
+              key={id}
+              username={post.username}
+              caption={post.caption}
+              imageUrl={post.imageUrl}
+            />
+          ))}
+        </div>
+        <div className="app__postsRight">
+          <h1>Hello</h1>
+          <InstagramEmbed
+            url="https://www.instagram.com/p/CREnUs4DJ8A/"
+            maxWidth={320}
+            clientAccessToken='EAAHa6FrplMMBALNsI0pbJ2I9C6ivlCaZCHKvgON1G4sMxtDRGPPBzvmluWZCO2P4cSTCn2DgrbojL6PuiH2jueAwCOXVRb1g4pwooNOCVwZAF2liDZCM5ZB12ge4OI1hzbF2ag3hjUTZCZA7FYK3HZAFFmtj7ZAfgZBViu9kBktF7KTbktRpCXhQhMsdqrtTZAKPZCieKFIVn1TQ7tyW5Iac02dcz0T0rmVRoZCevfVZCZCWxHMWCEmPvRwUvbH'
+            hideCaption={false}
+            containerTagName="div"
+            protocol=""
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+        </div>
+      </div>
 
       {user?.displayName ? (
         <ImageUpload username={user.displayName} />
       ) : (
         <h3>Sorry you need to login to upload</h3>
       )}
-
     </div>
   );
 }
